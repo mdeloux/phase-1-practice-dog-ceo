ed6ddc3198731276d8563a9894127c9dd4e18dc9
matt <mdeloux@gmail.com>
2022-01-21 19:35:53 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('%c HI', 'color: firebrick')
+
+
+
+function loadIMG() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
+        .then(response => response.json())
+        .then(json => renderImg(json))
+
+}
+
+function renderImg(images) {
+    const loc = document.querySelector('div')
+    images.message.forEach(Image => {
+        const img = document.createElement('img');
+        img.src = Image;
+        loc.appendChild(img)
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadIMG()
+})
+
+function dogBreeds() {
+    return fetch('https://dog.ceo/api/breeds/list/all')
+        .then(response => response.json())
+        .then(data => renderBreed(data))
+}
+function renderBreed(data) {
+    const ul = document.getElementById('dog-breeds');
+    for (dog in data) {
+        const li = document.createElement('li')
+        const node = document.createTextNode(dog);
+        li.appendChild(node);
+        ul.appendChild(li);
+    }
+}
+dogBreeds();
+
